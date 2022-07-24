cb37ed784fb63efb14504320f233ed77960bdbc6
rehan mulakhel <rehan.mulakhel@epfl.ch>
2022-07-24 15:02:59 +0200

    Add MathJax.

--- src/features/article/ArticleView.js
+++ src/features/article/ArticleView.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DOMPurify from 'dompurify';
 import { marked } from 'marked';
+import { MathJax, MathJaxContext } from 'better-react-mathjax';
 import { ArticleList } from './ArticleList';
 import { useGetArticleQuery } from '../../services/article';
 import './ArticleView.scss';
@@ -10,6 +11,14 @@ export const ArticleView = () => {
   const params = useParams();
   const [contentHtml, setContentHtml] = useState('');
   const { data, error, isLoading } = useGetArticleQuery(params.slug);
+
+  const config = {
+    loader: { load: ['[tex]/html'] },
+    tex: {
+      inlineMath: [['$', '$']],
+      displayMath: [['$$', '$$']],
+    },
+  };
 
   useEffect(() => {
     if (!error && !isLoading) {
@@ -26,7 +35,11 @@ export const ArticleView = () => {
       ) : data ? (
         <>
           <h1>{data.name}</h1>
-          <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+          <MathJaxContext version={3} config={config}>
+            <MathJax>
+              <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+            </MathJax>
+          </MathJaxContext>
           <aside>
             <hr />
             <h2>Autres articles</h2>
